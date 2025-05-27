1373998154dbd5fce5bf6b3a87ffdaeee210053d
Andreas Cardeneo <andreas@cardeneo.de>
2025-05-27 13:00:35 +0200

    Some changes to update file system data only if needed and refactorings

--- 2_ImGui/My_FileExplorer/src/render.cpp
+++ 2_ImGui/My_FileExplorer/src/render.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
-#include <string>
 #include <regex>
 
 #include <fmt/format.h>
 #include <imgui.h>
-#include <implot.h>
 
 #include "render.hpp"
 
 namespace
 {
-    std::string PathToString(const fs::path &pth)
+    std::string PathToString(const fs::path& pth)
     {
         std::string escaped = std::regex_replace(pth.string(), std::regex("\\\\"), "\\");
         return escaped;
+    }
+
+    std::vector<FolderContentEntry> FolderContent(const fs::path& pth)
+    {
+        std::vector<FolderContentEntry> output_lines;
+
+        for (const auto &entry : fs::directory_iterator(pth))
+            output_lines.emplace_back(entry, entry.is_directory());
+        return output_lines;
     }
 }; // namespace
 
@@ -24,32 +31,38 @@ void WindowClass::Draw(std::string_view label)
     if (ImGui::Button("[..Up..]"))
     {
         if (m_currentPath.has_parent_path())
-            m_currentPath = m_currentPath.parent_path();
+            updatePath(m_currentPath.parent_path());
     }
     ImGui::SameLine(); // next element appears in the same line!
     std::string pathString{PathToString(m_currentPath)};
     ImGui::Text("Current folder: %s", pathString.c_str());
     ImGui::Separator();
 
-    for (const auto& entry: fs::directory_iterator(m_currentPath))
-    {
-        std::string entry_name{entry.path().filename().string()};
-        std::string output_line;
-
-        if (entry.is_directory())
-        {
-            output_line = std::string{"[D] "}.append(entry_name);
-        }
-        else if (entry.is_regular_file())
-        {
-            output_line = std::string{"[F] "}.append(entry_name);
-        }
-        ImGui::Text(output_line.c_str());
-    }
+    DrawFolderContent();
 
     ImGui::Separator();
 
     ImGui::End();
+}
+
+void WindowClass::DrawFolderContent()
+{
+    if (m_needsUpdate)
+    {
+        m_folderContent = FolderContent(m_currentPath);
+        m_needsUpdate = false; // reset the flag
+    }
+    for (const auto &entry : m_folderContent)
+    {
+        bool is_selected;
+        bool entry_is_directory{entry.isDirectory()};
+
+        if (ImGui::Selectable(entry.label().c_str()) && entry_is_directory)
+        {
+            m_selectedPath = m_currentPath;
+            updatePath(m_currentPath /= entry.name());
+        }
+    }
 }
 
 void render(WindowClass &window_obj)
--- 2_ImGui/My_FileExplorer/src/render.hpp
+++ 2_ImGui/My_FileExplorer/src/render.hpp
@@ -2,18 +2,56 @@
 
 #include <string_view>
 #include <filesystem>
+#include <vector>
+#include <string>
 
 namespace fs = std::filesystem;
+
+class FolderContentEntry
+{
+public:
+    FolderContentEntry() = delete;
+
+    FolderContentEntry(const fs::path &path, bool isDirectory) : m_isDirectory(isDirectory), m_path(path)
+    {}
+
+    std::string label() const
+    {
+        if (m_isDirectory)
+            return std::string{"[D] "}.append(name());
+        else 
+            return std::string{"[F] "}.append(name());
+    }
+
+    std::string name() const { return m_path.filename().string(); }
+    fs::path path() const { return m_path; }
+    bool isDirectory() const { return m_isDirectory; }
+
+private:
+    bool m_isDirectory;
+    fs::path m_path;
+};
 
 class WindowClass
 {
 public:
-    WindowClass() : m_currentPath(fs::current_path()) {};
+    WindowClass() : m_currentPath(fs::current_path()), m_selectedPath(fs::current_path()), m_needsUpdate(true) {};
 
     void Draw(std::string_view label);
 
+    void DrawFolderContent();
+
 private:
     fs::path m_currentPath;
+    fs::path m_selectedPath;
+    bool m_needsUpdate;
+    std::vector<FolderContentEntry> m_folderContent;
+
+    void updatePath(const fs::path& newPath)
+    {
+        m_currentPath = newPath;
+        m_needsUpdate = true;
+    }
 };
 
 void render(WindowClass &window_obj);
